7941a366b9ac59938511ad7a93fb815c8f3c7a0e
DellaMoraWalter <53078750+DellaMoraWalter@users.noreply.github.com>
2020-01-30 19:04:23 +0100

    Add files via upload

---
+++ UI/UI/CustomControls/CustomButton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Prism;
+using Prism.Commands;
+
+using Syncfusion.XForms.Buttons;
+
+
+namespace UI.CustomControls
+{
+    //[DesignTimeVisible(true)]
+    //[XamlCompilation(XamlCompilationOptions.Compile)]
+    public class CustomButton : SfButton
+    {
+        public CustomButton()
+        {
+        }
+    }
+}
